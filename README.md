be6dc79e364c18fe2f33e56e937607d88fcfff4c
Meowcolm024 <luoyicong1026@live.com>
2019-02-02 08:09:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Euler
+
+Trying to answer the questions in [Project Euler](https://projecteuler.net)
+
+Most of the codes will be in __Haskell__, but some of them maybe in C++.
---
+++ e7.hs
@@ -0,0 +1,22 @@
+isPrime :: Int -> Bool
+isPrime k
+    | k <= 1    = error "Seriously?"
+    | otherwise = null [ x | x <- [2..floor(sqrt(fromIntegral k))], k `mod` x == 0]
+
+getPrime :: Int -> Int -> Int -> Int
+getPrime last this max
+    | prime && not reach = getPrime this (this+1) (max-1)
+    | prime && reach = this
+    | otherwise = getPrime last (this+1) max
+    where prime = isPrime this
+          reach = max == 0
+
+truePrime :: Int -> Int
+truePrime 1 = 2
+truePrime 2 = 3
+truePrime k = getPrime 2 3 (k-2)
+
+main::IO()
+main = do
+    let x = truePrime 10001
+    print x
