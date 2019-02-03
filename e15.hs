1ab23bc2884ca51da18b1023a5905d4f4838ec03
Meowcolm024 <luoyicong1026@live.com>
2019-02-03 23:07:18 +0800

    another two

--- e15.hs
+++ e15.hs
@@ -1,12 +1,13 @@
 -- Lattice paths
+
 import Data.List
 findposition number = (\(Just i)->i) . findIndex (==number)
-getposition number = head . drop number
+getposition number  = head . drop number
 
 count :: [(Int, Int)] -> [Int] -> (Int,Int) ->  Int
 count ps qs (x,y) 
     | x == 0 || y == 0                 = 1
-    | (0 < y || 0 < x) && c1 && c2     = getposition (findposition (x,y-1) ps) qs + getposition (findposition (x-1,y) ps) qs 
+    | (0 < y || 0 < x) && c1 && c2     = getposition (findposition (x,y-1) ps) qs + getposition (findposition (x-1,y) ps) qs
     | (0 < y || 0 < x) && c1 && not c2 = getposition (findposition (x,y-1) ps) qs + count ps qs (x-1,y)
     | (0 < y || 0 < x) && c2 && not c1 = count ps qs (x,y-1) + getposition (findposition (x-1,y) ps) qs
     | otherwise                        = count ps qs (x-1,y) + count ps qs (x,y-1)
@@ -20,6 +21,6 @@ latt m n xs
     where lp = [(x,y) | x <- [1..n-1], y <- [1..n-1]]
           p  = [(x,y) | x <- [1..n], y <- [1..n]]
           q  = map (count lp xs) p 
-          
+
 main::IO()
 main = print (last (latt 20 1 []))
---
+++ e17.hs
@@ -0,0 +1,13 @@
+-- Number letter counts
+
+a = length "onetwothreefourfivesixseveneightnine"
+b = length "teneleventwelvethirteenfourteenfifteensixteenseventeeneighteennineteen"
+c = length "twentythirtyfortyfiftysixtyseventyeightyninety"
+
+-- Expecting a program?
+-- Just take a pencil and a piece of paper!
+-- sum of 1~99: a*9+b+c*10 = 854
+-- 1~99 appears 10 times, 1~9 appears extra 100 times
+-- "hundred" appears 900 times, "and" appears 891 time
+-- and "one thousand" appears one time
+-- Just add them up :) 
