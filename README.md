cf156c6dd96f68af8e7f52fbe24f5789acf9eb24
Meowcolm024 <luoyicong1026@live.com>
2020-09-01 08:15:33 +0800

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Euler
+# Project Euler
+
+<img style="float: right;" src="https://projecteuler.net/profile/ervrge.png">
 
 Trying to answer the questions in [Project Euler](https://projecteuler.net)
 
-Most of the codes will be in __Haskell__, but some of them maybe in Python.
+Most of the codes will be in **Haskell**, but some of them maybe in **Python**.
+
--- e67.hs
+++ e67.hs
@@ -1,15 +1,13 @@
-
 maxTri :: [[Int]] -> Int
 maxTri [] = 0
 maxTri [x] = maximum x
-maxTri (x:y:xs) = maxTri (res:xs)
-    where
-        left = zipWith (+) (init x) y
-        right = zipWith (+) (tail x) y
-        res = map (\(a,b) -> max a b) $ zip left right
+maxTri (x : y : xs) = maxTri (zipWith max left right : xs)
+  where
+    left = zipWith (+) (init x) y
+    right = zipWith (+) (tail x) y
 
 main :: IO ()
 main = do
-    contents <- readFile "e67.txt"
-    let tmp = map (map read . words) . lines $ contents :: [[Int]]
-    print $ maxTri (reverse tmp)
+  contents <- readFile "e67.txt"
+  let tmp = map (map read . words) . lines $ contents :: [[Int]]
+  print $ maxTri (reverse tmp)
